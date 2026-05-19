7855bb95e187b0abe790db41b2ad00709a64a15d
Kennedy F. <kennedypaige126@gmail.com>
2026-05-19 05:39:39 -0500

    data: separated and cleaned code that had multiple statements on one line

--- task4_loops.py
+++ task4_loops.py
@@ -9,27 +9,38 @@ video_game_sales = [ [1, 'Wii Sports', 'Wii', 2006, 'Sports', 'Nintendo', 41.49,
 RANK = 0 NAME = 1 PLATFORM = 2 YEAR = 3 GENRE = 4 PUBLISHER = 5 NA_SALES = 6 EU_SALES = 7 JP_SALES = 8 GLOBAL_SALES = 9
 
 #a) Print games with more than 25 million global sales
-for game in video_game_sales: if game[GLOBAL_SALES] > 25: print(game[NAME], "-", game[GLOBAL_SALES], "million")
+for game in video_game_sales: 
+if game[GLOBAL_SALES] > 25: 
+  print(game[NAME], "-", game[GLOBAL_SALES], "million")
 
 #b) Count games released before 2000
 pre_2000_count = 0
 
-for game in video_game_sales: if game[YEAR] < 2000: pre_2000_count += 1
+for game in video_game_sales: 
+if game[YEAR] < 2000: pre_2000_count += 1
 
 print("Games released before 2000:", pre_2000_count)
 
 #c) Calculate total NA sales and JP sales
 total_na_sales = 0 total_jp_sales = 0
 
-for game in video_game_sales: total_na_sales += game[NA_SALES] total_jp_sales += game[JP_SALES]
+for game in video_game_sales: 
+  total_na_sales += game[NA_SALES] total_jp_sales += game[JP_SALES]
 
-print("Total North America sales:", total_na_sales) print("Total Japan sales:", total_jp_sales)
+print("Total North America sales:", total_na_sales) 
+print("Total Japan sales:", total_jp_sales)
 
-if total_na_sales > total_jp_sales: print("North America had higher sales.") elif total_jp_sales > total_na_sales: print("Japan had higher sales.") else: print("Sales were equal.")
+if total_na_sales > total_jp_sales: 
+  print("North America had higher sales.") 
+elif total_jp_sales > total_na_sales: print("Japan had higher sales.") 
+  else: 
+    print("Sales were equal.")
 
 #d) Create a list of Nintendo games
 nintendo_games = []
 
-for game in video_game_sales: if game[PUBLISHER] == 'Nintendo': nintendo_games.append(game[NAME])
+for game in video_game_sales: 
+if game[PUBLISHER] == 'Nintendo': nintendo_games.append(game[NAME])
 
-print(nintendo_games) print("Number of Nintendo games:", len(nintendo_games))
+print(nintendo_games) 
+print("Number of Nintendo games:", len(nintendo_games))
